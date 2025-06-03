f9ee41b1df7d5038e5d23a47db3489f9fe0a2e48
Sunish <sunishptpm@gmail.com>
2025-06-03 23:36:06 +0530

    build the version for production third  time

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sunishptpm/nrj/',
+  base: 'https://sunishptpm.github.io/nrj/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
